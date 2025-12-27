75534a46bce9ea1b42798b8c605f3a474912197e
Timur <74727399+Tmi-creator@users.noreply.github.com>
2025-12-27 06:07:46 +0300

    Route combat deathrattles through event system

--- engine/combat.py
+++ engine/combat.py
@@ -1,11 +1,14 @@
 from .entities import Unit, Player
-from .configs import UnitType
 import random
+
+from .effects import TRIGGER_REGISTRY
+from .event_system import Event, EventManager, EventType, TargetRef, TriggerInstance, Zone
 
 
 class Combat_Manager:
     def __init__(self):
         self.uid = 10000
+        self.event_manager = EventManager(TRIGGER_REGISTRY)
 
     def get_uid(self):
         self.uid += 1
@@ -14,6 +17,28 @@ class Combat_Manager:
     def resolve_combat(self, player_1, player_2):
         board_1 = [u.combat_copy() for u in player_1.board]
         board_2 = [u.combat_copy() for u in player_2.board]
+        combat_players = {
+            player_1.uid: player_1.__class__(
+                uid=player_1.uid,
+                board=board_1,
+                hand=[],
+                store=[],
+                tavern_tier=player_1.tavern_tier,
+                gold=0,
+                health=player_1.health,
+                up_cost=player_1.up_cost,
+            ),
+            player_2.uid: player_2.__class__(
+                uid=player_2.uid,
+                board=board_2,
+                hand=[],
+                store=[],
+                tavern_tier=player_2.tavern_tier,
+                gold=0,
+                health=player_2.health,
+                up_cost=player_2.up_cost,
+            ),
+        }
 
         boards = [board_1, board_2]
 
@@ -58,7 +83,7 @@ class Combat_Manager:
 
                 self.perform_attack(attacker_unit, target)
 
-                self.cleanup_dead(boards, attack_indices)
+                self.cleanup_dead(boards, attack_indices, combat_players)
 
                 if not attacker_unit.is_alive:
                     break
@@ -97,29 +122,20 @@ class Combat_Manager:
                 attacker.cur_hp = 0
                 target.has_venomous = False
 
-    def get_spawns(self, unit, owner_id):
-        """Возвращает список Unit, которые должны появиться после смерти"""
-        spawns = []
+    def _collect_death_triggers(self, unit, slot_index):
+        trigger_defs = self.event_manager.trigger_registry.get(unit.card_id, [])
+        triggers = []
+        for trigger_def in trigger_defs:
+            if trigger_def.event_type == EventType.MINION_DIED:
+                triggers.append(
+                    TriggerInstance(
+                        trigger_def=trigger_def,
+                        trigger_ref=TargetRef(side=unit.owner_id, zone=Zone.BOARD, slot=slot_index),
+                    )
+                )
+        return triggers
 
-        # 1. Обработка Deathrattle (по ID карты)
-        # Пример: Scallywag id="103" призывает пирата "103t"
-        if unit.card_id == "103":
-            token = Unit.create_from_db("103t", self.get_uid(), owner_id)
-            spawns.append(token)
-
-        elif unit.card_id == "108":  # Imprisoner
-            imp = Unit.create_from_db("108t", self.get_uid(), owner_id)
-            spawns.append(imp)
-
-        if unit.has_reborn:
-            reborn_unit = Unit.create_from_db(unit.card_id, self.get_uid(), owner_id)
-            reborn_unit.cur_hp = 1
-            reborn_unit.has_reborn = False
-            spawns.append(reborn_unit)
-
-        return spawns
-
-    def cleanup_dead(self, boards, attack_indices):
+    def cleanup_dead(self, boards, attack_indices, combat_players):
         """
         Чистим стол при смерти существ и двигаем индекс атаки куда надо
         """
@@ -130,20 +146,31 @@ class Combat_Manager:
                 unit = board[i]
 
                 if not unit.is_alive:
-                    new_units = self.get_spawns(unit, unit.owner_id)
+                    death_event = Event(
+                        event_type=EventType.MINION_DIED,
+                        source=TargetRef(side=unit.owner_id, zone=Zone.BOARD, slot=i),
+                    )
+                    extra_triggers = self._collect_death_triggers(unit, i)
 
                     board.pop(i)
 
                     if i < attack_indices[p_idx]:
                         attack_indices[p_idx] -= 1
 
-                    units_added = 0
-                    for new_u in new_units:
-                        if len(board) < 7:
-                            board.insert(i + units_added, new_u)
-                            units_added += 1
-                        else:
-                            break
+                    before_len = len(board)
+                    self.event_manager.process_event(
+                        death_event,
+                        combat_players,
+                        self.get_uid,
+                        extra_triggers=extra_triggers,
+                    )
+                    units_added = len(board) - before_len
+                    if unit.has_reborn and len(board) < 7:
+                        reborn_unit = Unit.create_from_db(unit.card_id, self.get_uid(), unit.owner_id)
+                        reborn_unit.cur_hp = 1
+                        reborn_unit.has_reborn = False
+                        board.insert(i, reborn_unit)
+                        units_added += 1
 
                     if i < attack_indices[p_idx]:
                         attack_indices[p_idx] += units_added

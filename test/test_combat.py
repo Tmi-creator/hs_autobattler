669d34b5b8c131c75f0f79721ffbf069dc1f2c49
Timur <74727399+Tmi-creator@users.noreply.github.com>
2026-01-18 22:18:40 +0300

    fixed some bugs

--- test/test_combat.py
+++ test/test_combat.py
@@ -1,10 +1,11 @@
 from engine.combat import CombatManager
 from engine.entities import Unit, Player
+from engine.configs import CARD_DB, CardIDs
+board1 = [Unit.create_from_db(CardIDs.ANNOY_O_TRON, 1, 1)]
+board2 = [Unit.create_from_db(CardIDs.SCALLYWAG, 2, 2)]
 
-board1 = [Unit.create_from_db("103", 1, 1)]
-board2 = [Unit.create_from_db("101", 2, 2)]
-
-Player1 = Player(uid=1, board=board1, hand=[], tavern_tier=1)
-Player2 = Player(uid=2, board=board2, hand=[], tavern_tier=1)
+Player1 = Player(uid=1, board=board1, hand=[])
+Player2 = Player(uid=2, board=board2, hand=[])
 combat_manager = CombatManager()
+print(combat_manager.resolve_combat(Player1, Player2))
 print(combat_manager.resolve_combat(Player1, Player2))
